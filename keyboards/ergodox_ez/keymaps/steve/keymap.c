2548ebbe9e8e79e509e5de26d4198480aac15cc2
Steffen Weinreich <steve@weinreich.org>
2018-08-25 17:55:28 +0200

    Reformat keymap, add Macros

--- keyboards/ergodox_ez/keymaps/steve/keymap.c
+++ keyboards/ergodox_ez/keymaps/steve/keymap.c
@@ -5,25 +5,111 @@
 #include "keymap_german.h"
 #include "keymap_nordic.h"
 
-#define KEYMAP LAYOUT_ergodox
+// A 'transparent' key code (that falls back to the layers below it).
+#define ___ KC_TRANSPARENT
+
+// A 'blocking' key code. Does nothing but prevent falling back to another layer.
+#define XXX KC_NO
+
+#define BASE 0
+#define SYMB 1
+#define GERM 2
+#define MEDI 3
 
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
   EPRM,
   VRSN,
   RGB_SLD,
-  
+  NAME,
+  MAIL,
+  WORK,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [0] = KEYMAP(KC_ESCAPE,KC_1,KC_2,KC_3,KC_4,KC_5,KC_6,KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,TG(3),MO(2),KC_A,KC_S,KC_D,KC_F,KC_G,KC_LSHIFT,DE_Y,KC_X,KC_C,KC_V,KC_B,ALL_T(KC_NO),KC_LCTL,KC_LGUI,KC_LALT,TT(1),TT(3),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_SPACE,KC_BSPACE,KC_TRANSPARENT,KC_7,KC_8,KC_9,KC_0,DE_SS,DE_ACUT,KC_BSPACE,TG(3),DE_Z,KC_U,KC_I,KC_O,KC_P,DE_PLUS,KC_H,KC_J,KC_K,KC_L,DE_HASH,KC_ENTER,MEH_T(KC_NO),KC_N,KC_M,KC_COMMA,KC_DOT,KC_UP,KC_RSHIFT,KC_RALT,LT(1,DE_MINS),KC_LEFT,KC_DOWN,KC_RIGHT,KC_PGUP,CTL_T(KC_ESCAPE),KC_PGDOWN,KC_DELETE,KC_DELETE,KC_SPACE),
+  [BASE] = LAYOUT_ergodox(
+		       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
+		       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(3),
+		       MO(2),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+		       KC_LSFT, DE_Y,    KC_X,    KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),
+		       KC_LCTL, KC_LGUI, KC_LALT, TT(1),   TT(3),
 
-  [1] = KEYMAP(KC_ESCAPE,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,KC_TRANSPARENT,KC_EXLM,KC_AT,KC_LCBR,KC_RCBR,KC_PIPE,KC_TRANSPARENT,KC_TRANSPARENT,KC_HASH,KC_DLR,KC_LPRN,KC_RPRN,KC_GRAVE,KC_TRANSPARENT,KC_PERC,KC_CIRC,KC_LBRACKET,KC_RBRACKET,KC_TILD,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,RGB_MOD,KC_TRANSPARENT,KC_TRANSPARENT,RGB_VAD,RGB_VAI,KC_TRANSPARENT,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_F12,KC_TRANSPARENT,KC_TRANSPARENT,KC_UP,KC_7,KC_8,KC_9,KC_ASTR,KC_TRANSPARENT,KC_DOWN,KC_4,KC_5,KC_6,KC_PLUS,KC_TRANSPARENT,KC_TRANSPARENT,KC_AMPR,KC_1,KC_2,KC_3,KC_BSLASH,KC_TRANSPARENT,KC_TRANSPARENT,KC_DOT,KC_0,KC_EQUAL,KC_TRANSPARENT,RGB_TOG,RGB_SLD,KC_TRANSPARENT,KC_TRANSPARENT,RGB_HUD,RGB_HUI),
+		                ___,     ___,
+		                         ___,
+		       KC_SPC,  KC_BSPC, ___,
 
-  [2] = KEYMAP(DE_CIRC,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,KC_TRANSPARENT,DE_AT,KC_TRANSPARENT,DE_EURO,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,DE_AE,DE_SS,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,DE_LESS,DE_PIPE,DE_MORE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_F12,KC_DELETE,KC_TRANSPARENT,KC_TRANSPARENT,DE_UE,KC_TRANSPARENT,DE_OE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_PGUP,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_PGDOWN,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT),
+		       KC_7,    KC_8,    KC_9,    KC_0,    DE_SS,   DE_ACUT, KC_BSPACE,
+		       TG(3),   DE_Z,    KC_U,    KC_I,    KC_O,    KC_P,    DE_PLUS,
+		       KC_H,    KC_J,    KC_K,    KC_L,    DE_HASH, KC_ENTER,MEH_T(KC_NO),
+		       KC_N,    KC_M,    KC_COMMA,KC_DOT,  KC_UP,   KC_RSHIFT,
+		       KC_RALT,LT(1,DE_MINS),KC_LEFT,KC_DOWN,KC_RIGHT,
+		       
+		       KC_PGUP,CTL_T(KC_ESCAPE),
+		       KC_PGDOWN,
+		       KC_DEL,  KC_DEL, KC_SPACE
+		       ),
 
-  [3] = KEYMAP(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_UP,KC_TRANSPARENT,KC_MS_WH_UP,KC_MS_ACCEL0,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT,KC_MS_WH_DOWN,KC_MS_ACCEL1,KC_TRANSPARENT,KC_MS_BTN1,KC_MS_BTN3,KC_MS_BTN2,KC_TRANSPARENT,KC_MS_ACCEL2,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_AUDIO_MUTE,KC_AUDIO_VOL_UP,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_AUDIO_VOL_DOWN,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_MEDIA_PLAY_PAUSE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_PGUP,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_PGDOWN,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_WWW_BACK),
+  [SYMB] = LAYOUT_ergodox(
+		       KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
+		       ___,     KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, ___,
+		       ___,     KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRAVE,
+		       ___,     KC_PERC, KC_CIRC, KC_LBRACKET,KC_RBRACKET,KC_TILD,___,
+		       ___,     ___,     ___,     ___,     ___,
+
+		                RGB_MOD, ___,
+		                         ___,
+		       RGB_VAD, RGB_VAI, ___,
+
+		       KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   ___,
+		       ___,     KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR,  ___,
+		                KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS,  ___,
+		       ___,     KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLASH,___,
+		       ___,     KC_DOT,  KC_0,    KC_EQUAL,___,
+
+		       RGB_TOG,RGB_SLD,
+		       ___,
+		       ___,RGB_HUD,RGB_HUI),
+
+  [GERM] = LAYOUT_ergodox(
+		       DE_CIRC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,
+		       ___,     DE_AT,   NAME,    DE_EURO, MAIL,    ___,      ___,
+		       ___,     DE_AE,   DE_SS,   ___,     WORK,    ___,
+		       ___,     DE_LESS, DE_PIPE, DE_MORE, ___,     ___,      ___,
+		       ___,     ___,     ___,     ___,     ___,
+		       
+		                ___,     ___,
+		                         ___,
+		       ___,     ___,     ___,
+
+		       KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   KC_DELETE,
+		       ___,     ___,     DE_UE,   ___,     DE_OE,   ___,      ___,
+		                ___,     ___,     ___,     ___,     ___,      ___,
+		       ___,     ___,     ___,     ___,     ___,     KC_PGUP,  ___,
+		                         ___,     ___,     ___,     KC_PGDOWN,___,
+		       ___,     ___,
+		       ___,
+		       ___,     ___,     ___),
+
+  [MEDI] = LAYOUT_ergodox(
+		       ___,     ___,     ___,     ___,     ___,     ___,      ___,
+		       ___,     ___,     KC_MS_UP,___,  KC_MS_WH_UP,KC_MS_ACCEL0,___,
+		       ___,   KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT,KC_MS_WH_DOWN,KC_MS_ACCEL1,
+		       ___,  KC_MS_BTN1,KC_MS_BTN3,KC_MS_BTN2, ___, KC_MS_ACCEL2,___,
+		       ___,     ___,     ___,     ___,     ___,
+		       
+		                ___,     ___,
+		                         ___,
+		       ___,     ___,     ___,
+
+		       VRSN,    ___,     ___,     ___,     ___, KC_AUDIO_MUTE,KC_AUDIO_VOL_UP,
+		       ___,     ___,     ___,     ___,     ___,     ___,      KC_AUDIO_VOL_DOWN,
+		                ___,     ___,     ___,KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_MEDIA_PLAY_PAUSE,
+		       ___,     ___,     ___,     ___,     ___,     KC_PGUP,  ___,
+		                         ___,     ___,     ___,     KC_PGDOWN,___,
+		       ___,     ___,
+		       ___,
+		       ___,     KC_WFWD, KC_WWW_BACK),
 
 };
 
@@ -65,7 +151,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case RGB_SLD:
+  case NAME:
+      if (record->event.pressed) {
+        SEND_STRING ("Steffen Weinreich");
+      }
+      return false;
+      break;
+  case MAIL:
+      if (record->event.pressed) {
+	SEND_STRING ("steve"SS_RALT("q")"weinreich.org");
+      }
+      return false;
+      break;
+  case WORK:
+      if (record->event.pressed) {
+	SEND_STRING ("steffen.weinreich"SS_RALT("q")"mdex.de");
+      }
+      return false;
+      break;
+  case RGB_SLD:
       if (record->event.pressed) {
         rgblight_mode(1);
       }
